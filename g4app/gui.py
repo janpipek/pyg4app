d91f5fbbca634805518f0cfc1be1e025ea5c70d4
Jan Pipek <jan.pipek@gmail.com>
2015-05-03 23:11:34 +0200

    Few more GUI commands

--- g4app/gui.py
+++ g4app/gui.py
@@ -9,5 +9,12 @@ class Gui(MacroBuilder):
             '/gui/addMenu vis Visualization',
             '/gui/addButton vis "Initialize" "/vis/open OGLSQt"',
             '/gui/addButton vis "Draw Volume" "/vis/drawVolume"',
-            '/gui/addButton vis "Draw Axes" "/vis/scene/add/axes 0 0 0 1 m"'
+            '/gui/addButton vis "Draw Trajectories" "/vis/scene/add/trajectories"',
+            '/gui/addButton vis "Draw Axes" "/vis/scene/add/axes 0 0 0 1 m"',
+
+            '/gui/addMenu run Run',
+            '/gui/addButton run "BeamOn 1" "/run/beamOn 1"',
+            '/gui/addButton run "BeamOn 10" "/run/beamOn 10"',
+            '/gui/addButton run "BeamOn 100" "/run/beamOn 100"',
+            '/gui/addButton run "BeamOn 1000" "/run/beamOn 1000"',
         ]
